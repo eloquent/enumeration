615b4a4890b84866ff74575a41b2a107bd4f258f
Erin Millard <emwebdev@gmail.com>
2012-07-05 09:59:00 +1000

    Fix composer autoload path in test boostrap. Closes #10.

--- test/src/bootstrap.php
+++ test/src/bootstrap.php
@@ -13,7 +13,7 @@
 require __DIR__.'/paths.php';
 
 // use Composer for autoloading
-$autoloader = require LQNT_VENDOR_DIR.DIRECTORY_SEPARATOR.'.composer'.DIRECTORY_SEPARATOR.'autoload.php';
+$autoloader = require LQNT_VENDOR_DIR.DIRECTORY_SEPARATOR.'autoload.php';
 $autoloader->add('Eloquent', LQNT_TEST_SRC_DIR);
 
 // include Phake for improved mocking support
